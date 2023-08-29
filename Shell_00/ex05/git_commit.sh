eaccecb39705670d7ff2974c359640e591332026
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-08-29 11:55:14 +0800

    Update git_commit.sh

--- Shell_00/ex05/git_commit.sh
+++ Shell_00/ex05/git_commit.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 git log --format='%H' -n5
+
+#The first line is a shebang which says this script should be executed using the Bash shell
+#git log: view repo commit history
+#--format='%H': Specifies the format of the output and displays only the commit hashes
+#n5: displays the latest 5 commits
