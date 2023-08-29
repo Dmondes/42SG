0d99f0c76d17ef3afe6166c3667c84dcc0661596
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-08-29 15:55:33 +0800

    Update MAC.sh

--- Shell_01/ex04/MAC.sh
+++ Shell_01/ex04/MAC.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 ifconfig | grep 'ether' | awk '{print $2}'
-#ifconfig | grep ether | cut -c15- | cut -c 1-18
-#remove -a dont need to include inactive addresses
+
+
+
+#ifconfig: display information about network interfaces on Unix-like systems.
+
+#|: The pipe symbol takes the output of the ifconfig command and passes it as input to the next command.
+
+#grep 'ether': Filters the output of ifconfig to show only lines that contain the word "ether". In ifconfig output, the "ether" line typically displays the MAC address.
+
+#awk '{print $2}':The awk tool, it will print the MAC address, which is the second field(column) on the "ether" lines
