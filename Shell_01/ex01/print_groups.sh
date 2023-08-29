bd4fcfb35d526b0043ea3417c75d36a22763c81f
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-08-29 15:06:55 +0800

    Update print_groups.sh

--- Shell_01/ex01/print_groups.sh
+++ Shell_01/ex01/print_groups.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 id -Gn $FT_USER | tr ' ' ',' | tr -d "\n"
+
+
+#id -Gn $FT_USER: Retrieves the group names of the user specified by $FT_USER. 
+#-Gn options tell the id command to display group names $FT_USER is replaced with the actual user's name.
+# | symbol is a pipe: Using the command on the left side of the symbol as input to run the command on the right side.
+#tr ' ' ',': Replaces spaces with commas in the list.
+#tr -d "\n": Removes newline characters, make sure the entire output is on a single line.
