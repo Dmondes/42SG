ec368fffb8715577108e609f0ba952b26ac20dea
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-08-29 15:39:59 +0800

    Update count_files.sh

--- Shell_01/ex03/count_files.sh
+++ Shell_01/ex03/count_files.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 find . | wc -l
+
+
+#find .: This command searches for files and directories in the current directory (.) and its subdirectories.
+#|: The pipe symbol takes the output of the find command and passes it as input to the next command.
+#wc -l: Counts the number of lines in the input. The total number of lines indicates the total number of files and directories as each file/ directory is a line.
+
