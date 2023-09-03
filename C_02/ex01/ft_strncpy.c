4db86f3f9a06fa19699e4ae509ed6ea143e80659
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-03 19:43:00 +0800

    Update ft_strncpy.c

--- C_02/ex01/ft_strncpy.c
+++ C_02/ex01/ft_strncpy.c
@@ -15,3 +15,6 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }
+
+// strncpy: to copy a specified n number of characters from one string to another. Prevents buffer overflow where src is greater than dest
+//copies src to dest. If src < n, '\0' will be used to fill up the rest of the spaces
