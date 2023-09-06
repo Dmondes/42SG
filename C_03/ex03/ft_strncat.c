27d4691974a5c160559b4eb1d24a5e2035764818
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-06 21:51:52 +0800

    Update ft_strncat.c

--- C_03/ex03/ft_strncat.c
+++ C_03/ex03/ft_strncat.c
@@ -7,14 +7,16 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	j = 0;
 	while (dest[i] != '\0')
 	{
-		i ++;
+		i ++; // iterate till the end of dest
 	}
 	while (src[j] != '\0' && j < nb)
 	{
-		dest[i] = src[j];
+		dest[i] = src[j]; // append as long as j counter is less than nb
 		i ++;
 		j ++;
 	}
-	dest[i] = '\0';
+	dest[i] = '\0'; // last space for null-terminated char
 	return (dest);
 }
+// concatenate char from src to dest up to n characters, prevent buffer overflow
+
