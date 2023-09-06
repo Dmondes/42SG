3514bbc7f6b18991996d3d6f4f558b3429bb4e53
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-06 22:01:04 +0800

    Update ft_strstr.c

--- C_03/ex04/ft_strstr.c
+++ C_03/ex04/ft_strstr.c
@@ -5,15 +5,15 @@ char	*ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	j = 0;
-	while (str[i])
+	while (str[i]) 
 	{
-		while (str[i + j] == to_find[j] && to_find[j])
+		while (str[i + j] == to_find[j] && to_find[j]) // compare the char in to_find and the char in str
 			j ++;
-		if (to_find[j] == '\0')
+		if (to_find[j] == '\0') // substring is found in str
 			return (&str[i]);
-		j = 0;
-		i ++;
+		j = 0; // reset count
+		i ++; // iterate through str
 	}
-	return (NULL);
+	return (NULL); // if not found
 }
-// strstr: find first occurence of substring (*to_find) in string str)
+// strstr: find first occurence of substring (*to_find) in string str), returns to the pointer of the first char of substring in str
