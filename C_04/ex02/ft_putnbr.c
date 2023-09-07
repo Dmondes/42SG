4b15ef9f0fd02b00f1fcc859c1f456223f46f35a
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-07 10:43:36 +0800

    Update ft_putnbr.c

--- C_04/ex02/ft_putnbr.c
+++ C_04/ex02/ft_putnbr.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: delim <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/09 08:03:50 by delim             #+#    #+#             */
-/*   Updated: 2023/07/09 08:40:04 by delim            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -19,19 +7,20 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	long	n;
+	long	n; // this is to handle the smallest int value (-ve)
 
 	n = nb;
-	if (n < 0)
+	if (n < 0) // if n is -ve, print the -sign and convert n to positive
 	{
-		ft_putchar('-');
+		ft_putchar('-'); 
 		n = -n;
 	}
 	if (n < 10)
-		ft_putchar(n + '0');
+		ft_putchar(n + '0'); // if n is less than 10, just print the digit char
 	else
 	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		ft_putnbr(n / 10); // removing the last digit, recursion till the base case of a single digit
+		ft_putnbr(n % 10); // get the remainder, last digit
 	}
 }
+// should be able to display all possible values within an int type variable, including -ve
