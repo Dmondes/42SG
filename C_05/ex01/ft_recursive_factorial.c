5858117747c5786329739e723a4b047b633e1973
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-07 15:58:18 +0800

    Update ft_recursive_factorial.c

--- C_05/ex01/ft_recursive_factorial.c
+++ C_05/ex01/ft_recursive_factorial.c
@@ -1,24 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: delim <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/07 08:41:49 by delim             #+#    #+#             */
-/*   Updated: 2023/07/07 08:58:22 by delim            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_recursive_factorial(int nb)
 {
 	int	result;
 
 	result = 1;
-	if (nb == 0 || nb == 1)
-		return (result);
+	if (nb == 0 || nb == 1) // returns 1 because the factorial of 0 and 1 is 1
+		return (result); 
 	if (nb > 1)
-		result = nb * ft_recursive_factorial(nb - 1);
+		result = nb * ft_recursive_factorial(nb - 1); // recursion till the base case
 	if (result > 1)
 		return (result);
 	else
