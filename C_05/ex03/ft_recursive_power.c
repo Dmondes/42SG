59028042a777f91fcc1c970796d8deedf6707220
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-08 15:16:58 +0800

    Update ft_recursive_power.c

--- C_05/ex03/ft_recursive_power.c
+++ C_05/ex03/ft_recursive_power.c
@@ -1,29 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_recursive_power.c                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: delim <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/07 09:22:09 by delim             #+#    #+#             */
-/*   Updated: 2023/07/07 09:28:05 by delim            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_recursive_power(int nb, int power)
 {
 	int	result;
 
 	result = nb;
 	if (power < 0)
-		return (0);
+		return (0); // return 0 if power is -ve 
 	if (power == 0)
-		return (1);
+		return (1); // base case of power is 0
 	if (power == 1)
-		return (nb);
+		return (nb); // base case of power is 1
 	else
 	{
-		result *= ft_recursive_power(nb, (power - 1));
+		result *= ft_recursive_power(nb, (power - 1)); // recursion
 		return (result);
 	}
 }
