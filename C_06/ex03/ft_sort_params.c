7c4cb7973bff3eb1a39c487f94f9953c9124d126
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-09 22:23:54 +0800

    Update ft_sort_params.c

--- C_06/ex03/ft_sort_params.c
+++ C_06/ex03/ft_sort_params.c
@@ -1,23 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: delim <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/09 14:35:40 by delim             #+#    #+#             */
-/*   Updated: 2023/07/10 11:10:04 by delim            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char c) // print character
 {
 	write(1, &c, 1);
 }
 
-void	print_str(char *str)
+void	print_str(char *str) // print entire string
 {
 	int	i;
 
@@ -30,7 +18,7 @@ void	print_str(char *str)
 	ft_putchar('\n');
 }
 
-int	compare_str(char *s1, char *s2)
+int	compare_str(char *s1, char *s2) // compares two strings s1 and s2 lexicographically
 {
 	int	i;
 
@@ -40,12 +28,12 @@ int	compare_str(char *s1, char *s2)
 		if (s1[i] == s2[i])
 			i ++;
 		if (s1[i] < s2[i] || s1[i])
-			return (s1[i] - s2[i]);
+			return (s1[i] - s2[i]); // -ve if s1 is less than s2, else +ve if s1 is more than s2
 	}
-	return (0);
+	return (0); // both are equal
 }
 
-void	swap(char **s1, char **s2)
+void	swap(char **s1, char **s2) // swap the arguments around
 {
 	char	*temp;
 
@@ -61,13 +49,13 @@ int	main(int argc, char **argv)
 
 	i = 0;
 	if (argc < 2)
-		return (0);
+		return (0); // nothing to print/ sort
 	if (argc == 2)
-		print_str(argv[1]);
+		print_str(argv[1]); // print 1 argument, no need to sort
 	while ((argc - 1) > i ++)
 	{
 		j = 1;
-		while (j < argc - i)
+		while (j < argc - i) // bubble sort
 		{
 			if (compare_str(argv[j], argv[j + 1]) > 0)
 			{
