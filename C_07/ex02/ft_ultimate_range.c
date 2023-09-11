48cfeda634c030c0970ec089d97d104faa216dc2
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-11 11:25:00 +0800

    Update ft_ultimate_range.c

--- C_07/ex02/ft_ultimate_range.c
+++ C_07/ex02/ft_ultimate_range.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-int	ft_ultimate_range(int **range, int min, int max)
+int	ft_ultimate_range(int **range, int min, int max) // range: pointer to a pointer of an int
 {
 	int	i;
 	int	diff;
@@ -10,14 +10,14 @@ int	ft_ultimate_range(int **range, int min, int max)
 	diff = max - min;
 	if (diff <= 0)
 	{
-		*range = 0;
+		*range = 0; // empty/ invalid range, points to 0 (NULL)
 		return (0);
 	}
-	array = (int *)malloc(sizeof(int) * diff);
+	array = (int *)malloc(sizeof(int) * diff); //reserve space to store all the integers
 	if (!array)
-		return (-1);
+		return (-1); // malloc faileed
 	while (i < diff)
 		array[i ++] = min ++ ;
-	*range = array;
+	*range = array; // assigns the address of the dynamically allocated array to *range
 	return (diff);
 }
