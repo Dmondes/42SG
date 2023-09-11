ffbef6131747bab4af92c1d2c8bef16221a6d489
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-11 12:02:26 +0800

    Update ft_strjoin.c

--- C_07/ex03/ft_strjoin.c
+++ C_07/ex03/ft_strjoin.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-
-int	ft_strlen(char *str)
+// This answer does not pass moulinette
+int	ft_strlen(char *str) // return length of string
 {
 	int	i;
 
@@ -16,13 +16,13 @@ int	ft_full_len(int size, char **strs, char *sep)
 	int	concat_len;
 
 	i = 0;
-	concat_len = ft_strlen(sep) * (size - 1);
+	concat_len = ft_strlen(sep) * (size - 1); // No of seps is 1 less than the no of strings
 	while (i < size)
 	{
-		concat_len += ft_strlen(strs[i]);
+		concat_len += ft_strlen(strs[i]); // add each length of string to count
 		i ++;
 	}
-	return (concat_len);
+	return (concat_len); // return total length required
 }
 
 void	ft_addstr(int size, char **strs, char *sep, char *concat_str)
@@ -37,13 +37,13 @@ void	ft_addstr(int size, char **strs, char *sep, char *concat_str)
 	{
 		j = 0;
 		while (strs[i][j])
-			concat_str[k ++] = strs[i][j ++];
-		j = 0;
-		while (sep[j] && i < (size - 1))
+			concat_str[k ++] = strs[i][j ++]; // add string
+		j = 0; //reset j
+		while (sep[j] && i < (size - 1)) // add sep between string
 			concat_str[k ++] = sep[j ++];
-		i ++;
+		i ++; //next string
 	}
-	concat_str[k] = '\0';
+	concat_str[k] = '\0'; // null-terminated at the end
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
@@ -51,15 +51,15 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	char	*concat_str;
 	int		concat_len;
 
-	if (size == 0)
+	if (size == 0) // check if size is 0
 	{
-		concat_str = (char *)malloc(sizeof(char));
+		concat_str = (char *)malloc(sizeof(char)); // allocate memory for empty string
 		return (concat_str);
 	}
-	concat_len = ft_full_len(size, strs, sep);
-	concat_str = (char *)malloc((concat_len + 1) * sizeof(char));
-	if (!concat_str)
+	concat_len = ft_full_len(size, strs, sep); // find the total length required
+	concat_str = (char *)malloc((concat_len + 1) * sizeof(char)); //Allocate memory for the concatenated string + null terminator
+	if (!concat_str) // if malloc fails
 		return (0);
-	ft_addstr(size, strs, sep, concat_str);
+	ft_addstr(size, strs, sep, concat_str); // fill up the spaces in the range
 	return (concat_str);
 }
