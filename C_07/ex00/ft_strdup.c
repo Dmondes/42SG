6040ed983bd585f4abc332eb9df3b0beb45913b9
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-11 10:41:10 +0800

    Update ft_strdup.c

--- C_07/ex00/ft_strdup.c
+++ C_07/ex00/ft_strdup.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	ft_strlen(char *str) // counts the length of string
 {
 	int	i;
 
@@ -18,14 +18,15 @@ char	*ft_strdup(char *src)
 
 	i = 0;
 	len = ft_strlen(src);
-	point = (char *)malloc(sizeof(char) * len);
+	point = (char *)malloc(sizeof(char) * len); // reserve the space required to store the string
 	if (!point)
-		return (0);
+		return (0); // if malloc fails, return 0
 	while (src[i])
 	{
 		point[i] = src[i];
 		i ++;
 	}
-	point[i] = '\0';
-	return (point);
+	point[i] = '\0'; // null-terminate the duplicate string
+	return (point); // returns the pointer to the start of the duplicate
 }
+// strdup: returns a dynamically allocated copy of a string, useful to modify duplicate without affecting the original
