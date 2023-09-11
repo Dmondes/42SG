65d26b75ecbf64e0e164f26d2e60f27cee7139b5
Desmond <106146898+Dmondes@users.noreply.github.com>
2023-09-11 10:56:08 +0800

    Update ft_range.c

--- C_07/ex01/ft_range.c
+++ C_07/ex01/ft_range.c
@@ -2,18 +2,18 @@
 
 int	*ft_range(int min, int max)
 {
-	int	i;
-	int	*range;
-	int	diff;
+	int	i; // index
+	int	*range; // array
+	int	diff; //counts no of int in the range
 
 	i = 0;
 	diff = max - min;
 	if (diff <= 0)
-		return (0);
-	range = (int *)malloc(sizeof(int) * diff);
+		return (0); // no int in range
+	range = (int *)malloc(sizeof(int) * diff); // reserve space to store the integers
 	if (!range)
-		return (0);
-	while (min < max)
+		return (0); // malloc failed
+	while (min < max) // max is excluded in range
 	{
 		range[i] = min;
 		min ++;
